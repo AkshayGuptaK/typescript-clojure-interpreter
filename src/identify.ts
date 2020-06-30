aecd5610d69dcbb79925a9968ca07f4b0ecc299f
Akshay Gupta <akshaygrkesh@gmail.com>
2020-06-30 23:41:33 +0530

    Identification helpers

---
+++ src/identify.ts
@@ -0,0 +1,20 @@
+const LETTER = /[a-zA-Z]/;
+const WHITESPACE = /\s+/;
+const NUMBER = /^[0-9]+$/;
+
+type test = (character: string) => boolean;
+
+export const isLetter: test = LETTER.test;
+
+export const isWhitespace: test = WHITESPACE.test;
+
+export const isNumber: test = NUMBER.test;
+
+export const isOpeningParanthesis: test = (character) => character === "(";
+
+export const isClosingParanthesis: test = (character) => character === ")";
+
+export const isParanthesis: test = (character) =>
+  isOpeningParanthesis(character) || isClosingParanthesis(character);
+
+export const isQuote: test = (character) => character === '"';
