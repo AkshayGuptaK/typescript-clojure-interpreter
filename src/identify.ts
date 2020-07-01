cd75654d761d710c74bbe86acd3fff2118ab6f34
Akshay Gupta <akshaygrkesh@gmail.com>
2020-07-01 21:04:05 +0530

    Revamped identify functions to chunk tokens

--- src/identify.ts
+++ src/identify.ts
@@ -1,20 +1,29 @@
-const LETTER = /[a-zA-Z]/;
-const WHITESPACE = /\s+/;
-const NUMBER = /^[0-9]+$/;
+const WHITESPACE = /^\s+/;
+const NUMBER = /^((+|-)?(\d+))([.]\d+)?([Ee][+-]?\d+)?/;
+const WORD = /^[a-zA-Z]+/;
 
-type test = (character: string) => boolean;
+type test = (input: string) => number;
 
-export const isLetter: test = LETTER.test;
+function regexMatchLength(regex: RegExp): test {
+  return (input) => {
+    const match = input.match(regex);
+    return match ? match[0].length : 0;
+  };
+}
 
-export const isWhitespace: test = WHITESPACE.test;
+export const matchWhitespace: test = regexMatchLength(WHITESPACE);
 
-export const isNumber: test = NUMBER.test;
+export const matchNumber: test = regexMatchLength(NUMBER);
 
-export const isOpeningParanthesis: test = (character) => character === "(";
+export const matchWord: test = regexMatchLength(WORD);
 
-export const isClosingParanthesis: test = (character) => character === ")";
+export const matchOpeningParanthesis: test = (input) =>
+  input.startsWith("(") ? 1 : 0;
 
-export const isParanthesis: test = (character) =>
-  isOpeningParanthesis(character) || isClosingParanthesis(character);
+export const matchClosingParanthesis: test = (input) =>
+  input.startsWith("(") ? 1 : 0;
 
-export const isQuote: test = (character) => character === '"';
+export const matchParanthesis: test = (input) =>
+  matchOpeningParanthesis(input) + matchClosingParanthesis(input);
+
+export const matchQuote: test = (input) => (input.startsWith('"') ? 1 : 0);
