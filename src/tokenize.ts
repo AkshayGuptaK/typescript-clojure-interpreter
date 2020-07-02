f6943418c157ea18d75108a0175bd5249996e5f1
Akshay Gupta <akshaygrkesh@gmail.com>
2020-07-02 08:21:04 +0530

    Basic tokenizer

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -18,13 +18,39 @@ export const tokenize = (input: string): Token[] => {
   while (cursor < input.length) {
     const character = input[cursor];
 
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
     if (isParanthesis(character)) {
       tokens.push({
         type: "Paranthesis",
         value: character,
       });
+      cursor++;
+      continue;
     }
-    cursor++;
+
+    if (isNumber(character)) {
+      tokens.push({
+        type: "Number",
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isLetter(character)) {
+      tokens.push({
+        type: "Letter",
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`Unrecognized character ${character}`);
   }
   return tokens;
 };
